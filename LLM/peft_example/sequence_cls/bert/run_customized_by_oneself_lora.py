934c29f2d5807bf9ea3f6e138a479d2e569050ed
shawroad <luxiaonlp@163.com>
2023-04-21 15:56:15 +0800

    customized

---
+++ LLM/peft_example/sequence_cls/bert/run_customized_by_oneself_lora.py
@@ -0,0 +1,27 @@
+"""
+@file   : run_customized_by_oneself_lora.py
+@author : xiaolu
+@email  : luxiaonlp@163.com
+@time   : 2023-04-21
+"""
+from transformers.models.bert import BertForSequenceClassification, BertTokenizer
+from peft import LoraConfig, get_peft_model
+
+model = BertForSequenceClassification.from_pretrained('./mengzi_pretrain')
+
+# # 查看当前网络中都有哪些模块
+# for x in model.modules():   # 查看模块
+#     print(x)
+
+# # 准备lora
+# 计算在那个模块上应用lora
+TARGET_MODULES = [
+    "query",
+    "key",
+]
+peft_config = LoraConfig(task_type="SEQ_CLS", target_modules=TARGET_MODULES, inference_mode=False, r=8, lora_alpha=16, lora_dropout=0.1)
+model = get_peft_model(model, peft_config)
+print(model.print_trainable_parameters())
+exit()
+
+
